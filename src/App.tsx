26e3dec2191d8fb3ba47b05078437b57c0540905
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-01-12 21:19:50 -0300

    feat: Create AudioOrVideoInput component
    - Create AudioOrVideoInput component to separate from App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import '@vidstack/react/player/styles/base.css'
 import { MediaPlayer, MediaPlayerInstance, MediaProvider } from '@vidstack/react'
 
 import Timer from './components/Timer'
-import Button from './components/Button'
+import AudioOrVideoInput from './components/AudioOrVideoInput'
 import StartOrPauseTimerButton from './components/StartOrPauseTimerButton'
+import Button from './components/Button'
 
 import useTimer from './hooks/useTimer'
 
@@ -107,30 +108,7 @@ export default function App() {
       </h1>
       <section className="flex flex-col items-center gap-12">
         <Timer timeLeft={timeLeft} />
-        <div className="flex flex-col gap-y-4">
-          <p className='opacity-50'>Em breve, será possível fazer o upload de arquivos de áudio ou vídeo.</p>
-          <div className="flex gap-4 hidden">
-            <div>
-              <input type="file" accept="audio/*, video/*" id="audio-upload" className="hidden" />
-              <label
-                htmlFor="audio-upload"
-                className="bg-blue-500 text-white py-2 px-4 rounded text-2xl font-[Inter] font-bold cursor-pointer"
-              >
-                Usar arquivo de áudio ou vídeo
-              </label>
-            </div>
-            <span className="text-2xl font-[Inter] font-semibold">ou</span>
-          </div>
-          <input
-            type="url"
-            className="
-              border border-gray-300 rounded py-2 px-4 text-2xl font-[Inter] font-semibold outline-none
-              focus:border-blue-500 focus:border focus:ring-2 focus:ring-blue-500
-            "
-            placeholder="Link do youtube"
-            onChange={handleYoutubeVideoUrlChange}
-          />
-        </div>
+        <AudioOrVideoInput handleYoutubeVideoUrlChange={handleYoutubeVideoUrlChange} />
         <div className="flex gap-4">
           <StartOrPauseTimerButton
             isRunning={isRunning}
