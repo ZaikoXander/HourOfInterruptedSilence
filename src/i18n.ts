546e17e60af965514ce22b1cc573ca902ed902d7
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-01-17 21:32:38 -0300

    feat(i18n): Add language detection
    - Add i18next-browser-languagedetector dependency
    
    - Rename pt_br.json file to pt-BR.json
    
    - Set html lang in index.html as pt-BR

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,18 @@
 import i18n from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next"
 
 import en from "./locales/en.json"
-import pt_br from "./locales/pt_br.json"
+import pt_br from "./locales/pt-BR.json"
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
-      pt_br: { translation: pt_br },
+      'pt-BR': { translation: pt_br },
     },
-    lng: "en",
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
