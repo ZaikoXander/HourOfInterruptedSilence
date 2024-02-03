49aa4a71f905af9518a6b116e6a1259619f5f98d
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-02-02 23:12:09 -0300

    style: Add prettier to the project and a react eslint plugin
    - Add new development dependencies:
      - 'eslint-config-prettier'
      - 'eslint-plugin-prettier'
      - 'eslint-plugin-react'
      - 'prettier'
      - 'prettier-plugin-tailwindcss'
    
    - Setup eslint config to extend prettier and react plugins
    
    - Add a react eslint plugin for improved react linting
    
    - Add a prettier configuration file:
      - Use tailwindcss prettier plugin, for class formatting
    
    - Add a format script to the package.json file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,24 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    }
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
   },
 }
