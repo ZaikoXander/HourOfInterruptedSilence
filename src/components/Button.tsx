6ed5f9548293d93d8c0f6c9254de22d8cffcc4e5
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-02-18 16:35:02 -0300

    feat(Button): Add small shadow

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,8 +20,8 @@ export default function Button({
       type='button'
       className={cn(
         'rounded bg-blue-500 px-4 py-2 font-[Inter] text-2xl font-bold',
-        'text-white transition-colors hover:bg-blue-600 hover:text-gray-100',
-        'disabled:opacity-50 disabled:hover:bg-blue-500',
+        'text-white shadow-sm shadow-black transition-colors hover:bg-blue-600',
+        'hover:text-gray-100 disabled:opacity-50 disabled:hover:bg-blue-500',
         'disabled:hover:text-white',
         className,
       )}
